3f5ceef3e782d724dc55668120ee9d1b3ce32d9c
Eum Young Hyun <opwer032@naver.com>
2022-02-02 23:03:44 +0900

    feat: 라우터 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
