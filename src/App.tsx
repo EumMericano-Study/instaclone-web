d92c23548da9f8cc115cba3852af4180b5383db6
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-02-04 13:15:49 +0900

    feat: ReactiveValue 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./screens/Home";
-import Login from "./screens/Login";
-import NotFound from "./screens/NotFound";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar, isDarkModeVar } from "apollo";
+import Home from "screens/Home";
+import Login from "screens/Login";
+import NotFound from "screens/NotFound";
 
 function App() {
-  const isLoggedIn = false;
+    const isLoggedIn = useReactiveVar(isLoggedInVar);
+    const isDarkMode = useReactiveVar(isDarkModeVar);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
