297920a8f08ac88423687418ff41979e7c668c63
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-02-04 12:59:41 +0900

    feat: Build reUsable Component

---
+++ src/components/elements/Container.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+function Container(props: Props) {
+    const { children } = props;
+    return <Wrapper>{children}</Wrapper>;
+}
+
+export default Container;
+
+const Wrapper = styled.div`
+    width: 100vw;
+    min-height: 100vh;
+`;
