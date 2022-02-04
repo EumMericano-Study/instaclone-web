2ea7e13c8599b8f7fd495469f963bcf7d25de8f7
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-02-04 13:20:42 +0900

    feat: 테마 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import { isLoggedInVar, isDarkModeVar } from "apollo";
 import Home from "screens/Home";
 import Login from "screens/Login";
 import NotFound from "screens/NotFound";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, lightTheme, darkTheme } from "styles";
 
 function App() {
     const isLoggedIn = useReactiveVar(isLoggedInVar);
     const isDarkMode = useReactiveVar(isDarkModeVar);
 
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
+        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+            <GlobalStyles />
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={isLoggedIn ? <Home /> : <Login />}
+                    />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
     );
 }
 
